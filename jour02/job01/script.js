15ce44a01768753a287d0a562d4e44528a226ae4
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-03-15 15:55:34 +0100

    job 2 shoowhide fini

---
+++ jour02/job01/script.js
@@ -0,0 +1,26 @@
+
+
+
+document.addEventListener("DOMContentLoaded" , () => {
+
+
+let article = document.getElementById('citation').textContent
+
+let btn = document.getElementById("button")
+
+console.log(btn)
+
+    function citation (){
+
+        btn.addEventListener("click", () => {
+        console.log(article)
+
+    })
+
+}
+citation ()
+
+});
+
+
+
